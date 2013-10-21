7192535576904225e301dd9adec413c842d5b8e1
jkwok91 <0111291@gmail.com>
2013-10-20 17:14:55 -0700

    initial commit
    Look at this drunken mess
    I love coding when I'm tired, wasted, and high.  It makes for laughs.

---
+++ scrape.py
@@ -0,0 +1,12 @@
+#get a python script to import the html file and parse it to scrape all inline styles
+
+dev scrapeStyles():
+  #introduce the celebs of the red lights they crosse and traffic lights they disregard
+  jessica = ur boss
+  page = something.readline()
+  for each line in page:
+    if line has the word style in braces:
+      take that out [from < til next </> and if there was a </ ft that then they lose whatever many { or here i guess
+
+@ unfortunately i am falling the fuck asleep right here
+
