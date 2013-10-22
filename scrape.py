33a03bdacf944067b2dbe5ab17803db869f81bf6
jkwok91 <0111291@gmail.com>
2013-10-22 14:40:55 -0700

    makes use of an actual parser
    no more drunk coding

--- scrape.py
+++ scrape.py
@@ -1,12 +1,42 @@
 #get a python script to import the html file and parse it to scrape all inline styles
 
-dev scrapeStyles():
-  #introduce the celebs of the red lights they crosse and traffic lights they disregard
-  jessica = ur boss
-  page = something.readline()
-  for each line in page:
-    if line has the word style in braces:
-      take that out [from < til next </> and if there was a </ ft that then they lose whatever many { or here i guess
+from HTMLParser import HTMLParser
 
-@ unfortunately i am falling the fuck asleep right here
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        #keep track of position in DOM
+        #i will use a stack.  every time i enter, i push the element onto the stack
+        self.posStakk = []
+    def handle_starttag(self, tag, attrs):
+        element = tag
+        styling = ''
+        for attr in attrs:
+          if attr[0] == "class":
+            element += "."+attr[1]
+          if attr[0] == "id":
+            element += "#"+attr[1]
+          if attr[0] == "style":
+            print attr[1]
+        self.posStakk.append(element)
+        return tag
+    def handle_endtag(self, tag):
+        self.posStakk.pop(-1)
+        return "end"
+    def handle_data(self, data):
+        print self.posStakk
+        print "Encountered some data  :", data
 
+def scrapeStyles():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+
+  #open file
+  page = open('asdf.html','r+')
+  
+  parser.feed(page.read())
+
+# unfortunately i am falling the fuck asleep right here
+
+scrapeStyles()
